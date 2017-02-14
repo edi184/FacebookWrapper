3f4c3424dfef7a33abed16894331915db270d91b
edi194 <misiakadrian@gmail.com>
2017-02-14 15:27:37 +0100

    Zoom when stickers choosed fix

--- app/src/main/java/edi194/messengerlite/MainActivity.java
+++ app/src/main/java/edi194/messengerlite/MainActivity.java
@@ -3,6 +3,7 @@ package edi194.messengerlite;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.webkit.WebSettings;
 import android.webkit.WebViewClient;
 
@@ -25,12 +26,14 @@ public class MainActivity extends Activity {
         mWebView.setWebViewClient(new WebViewClient());
         mWebView.getSettings().setSupportZoom(false);
         mWebView.setInitialScale(210);
+        mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
         mWebView.loadUrl("https://www.messenger.com/");
 
 
-    }
 
+    }
+    
     @Override
     public void onBackPressed() {
         if (mWebView.canGoBack()) {
