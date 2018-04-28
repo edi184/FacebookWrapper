118d591ec2f36843a8c4684968ae7eadfa1f7ecf
edi184 <misiakadrian@gmail.com>
2018-04-28 12:50:41 +0200

    little updates
    - add swipe down to refresh (stackoverflow love)

--- app/src/main/java/edi194/fblite/MainActivity.java
+++ app/src/main/java/edi194/fblite/MainActivity.java
@@ -1,37 +1,47 @@
 package edi194.fblite;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebSettings;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class MainActivity extends Activity {
-
-    private android.webkit.WebView mWebView;
+public class MainActivity extends AppCompatActivity {
+    WebView view;
+    SwipeRefreshLayout mySwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySwipeRefreshLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipeContainer);
+        String url = "http://m.facebook.com/";
+        view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.getSettings().setBuiltInZoomControls(true);
+        view.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.97");
+        view.getSettings().setDisplayZoomControls(false);
+        view.setWebViewClient(new WebViewClient());
+        view.loadUrl(url);
 
-        mWebView = findViewById(R.id.activity_main_webview);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebView.getSettings().setSupportZoom(false);
-        mWebView.getSettings().setLoadWithOverviewMode(true);
-        mWebView.loadUrl("https://m.facebook.com/");
-        mWebView.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.97");
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        view.reload();
+                        mySwipeRefreshLayout.setRefreshing(false);
+                    }
+                }
+        );
     }
 
-    @Override
-    public void onBackPressed() {
-        if (mWebView.canGoBack()) {
-            mWebView.goBack();
-        } else {
-            super.onBackPressed();
+        @Override
+        public void onBackPressed() {
+            if (view.canGoBack()) {
+                view.goBack();
+            } else {
+                super.onBackPressed();
+            }
         }
-    }
+
 }
