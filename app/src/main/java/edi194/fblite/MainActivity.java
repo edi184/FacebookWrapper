e6972e35511b5cfae4c7b2a6bb9933d61be3bdfe
edi184 <misiakadrian@gmail.com>
2018-05-01 11:16:24 +0200

    added save image option & little updates

--- app/src/main/java/edi194/fblite/MainActivity.java
+++ app/src/main/java/edi194/fblite/MainActivity.java
@@ -1,47 +1,95 @@
 package edi194.fblite;
 
+import android.app.DownloadManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-    WebView view;
+    WebView webView;
     SwipeRefreshLayout mySwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mySwipeRefreshLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipeContainer);
+        mySwipeRefreshLayout = this.findViewById(R.id.swipeContainer);
         String url = "http://m.facebook.com/";
-        view = (WebView) this.findViewById(R.id.webView);
-        view.getSettings().setJavaScriptEnabled(true);
-        view.getSettings().setBuiltInZoomControls(true);
-        view.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.97");
-        view.getSettings().setDisplayZoomControls(false);
-        view.setWebViewClient(new WebViewClient());
-        view.loadUrl(url);
+        webView = (WebView) this.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.97");
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+        registerForContextMenu(webView);
 
         mySwipeRefreshLayout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener() {
                     @Override
                     public void onRefresh() {
-                        view.reload();
+                        webView.reload();
                         mySwipeRefreshLayout.setRefreshing(false);
                     }
                 }
         );
     }
-
-        @Override
-        public void onBackPressed() {
-            if (view.canGoBack()) {
-                view.goBack();
-            } else {
-                super.onBackPressed();
-            }
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
         }
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo){
+        super.onCreateContextMenu(contextMenu, view, contextMenuInfo);
 
+        final WebView.HitTestResult webViewHitTestResult = webView.getHitTestResult();
+
+        if (webViewHitTestResult.getType() == WebView.HitTestResult.IMAGE_TYPE ||
+                webViewHitTestResult.getType() == WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE) {
+            Toast.makeText(MainActivity.this,R.string.info,Toast.LENGTH_LONG).show();
+            contextMenu.setHeaderTitle(R.string.save_title);
+            contextMenu.add(0, 1, 0, R.string.save_menu)
+                    .setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem menuItem) {
+
+                            String DownloadImageURL = webViewHitTestResult.getExtra();
+
+                            if(URLUtil.isValidUrl(DownloadImageURL)){
+
+                                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(DownloadImageURL));
+                                request.allowScanningByMediaScanner();
+                                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                                downloadManager.enqueue(request);
+
+                                Toast.makeText(MainActivity.this,R.string.save_done,Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this,R.string.save_failed,Toast.LENGTH_LONG).show();
+                            }
+                            return false;
+                        }
+                    });
+        }
+    }
 }
